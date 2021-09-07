dc2dbe9a1c97a9cdd2b2d460fc88ee7dfa34ac3f
Dillan1903 <A01252454@itesm.mx>
2021-09-07 03:28:42 +0000

    25trianguloasteriscos act dillan meza

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,7 +1,9 @@
 
 def main():
     #escribe tu código abajo de esta línea
-
+    h = int(input("Enter triangle heigh:"))
+    for i in range(1,h + 1):
+        print(''*(h - 1)+'*'*i)
 
 if __name__=='__main__':
     main()
